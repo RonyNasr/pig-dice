e481f7fd7d91ea2f13b016da810f38f063af7b1b
Igor Seleznov and Rony Nasr <igor@>
2016-06-15 10:17:28 -0700

    backend logic implemented

--- js/scripts.js
+++ js/scripts.js
@@ -21,20 +21,48 @@ Player.prototype.hold = function (total) {
   this.score += total;
 };
 
-// function Game(){
-//   this.total= 0
-//   this.players = [];
-// }
+Player.prototype.checkWin = function (total) {
+  return ((this.score + total) >= 100);
+};
+
+var player1 = new Player(null,0,1);
+var player2 = new Player (null,0,0);
+
+var switchPlayer = function(playerName){
+  console.log(player1.name, playerName);
+  if (player1.name === playerName){
+    player1.endTurn();
+    $("#score1").removeClass("active");
+    player2.startTurn()
+    $("#score2").addClass("active");
+  }else {
+    player2.endTurn();
+      $("#score2").removeClass("active");
+    player1.startTurn()
+      $("#score1").addClass("active");
+  }
+}
+
+var resetGame = function(){
+  player1.name = null;
+  player2.name = null;
+  $("input#player1").val("");
+  $("input#player2").val("");
+  $("#score1-span").text("0");
+  $("#score2-span").text("0");
+  $("#total-span").text("0");
+  $(".game-col").hide();
+}
 
 //Front end logic
 $(function(){
-  var total = 0;
-  var player1 = new Player(null,0,1);
-  var player2 = new Player (null,0,0);
+    var total = 0;
 
   $("#add").click(function(){
     player1.name = $("input#player1").val();
     player2.name = $("input#player2").val();
+
+    $("#score1").addClass("active");
 
     $("#p1-score").text(player1.name);
     $("#p2-score").text(player2.name);
@@ -47,38 +75,43 @@ $(function(){
 
   $("#roll").click(function(){
     //if it's the player's turn
+      // total = 0;
       var dice = getRandom(1, 6);
+      $("#dice-value").text(dice);
       if (dice !== 1){
         total +=dice;
         $("#total-span").text(total);
+        if (player1.turn && player1.checkWin(total)){
+          player1.score += total;
+          alert (player1.name + " has won!! <br> score: " + player1.score);
+          resetGame();
+        }else if (player2.turn && player2.checkWin(total)) {
+          player2.score += total;
+          alert (player2.name + " has won!!<br> score: " + player2.score);
+          resetGame()
+        }
       }else {
         total=0;
         $("#total-span").text(total);
         if (player1.turn){
-          player1.endTurn();
-          player2.startTurn()
+            switchPlayer(player1.name);
         }else {
-          player2.endTurn();
-          player1.startTurn()
+          switchPlayer(player2.name);
         }
       }
+    });
 
     $("#hold").click(function() {
-    //  debugger;
       if (player1.turn === 1){
         player1.score += total;
         $("#score1-span").text(player1.score);
         total = 0 ;
-        player1.endTurn();
-        player2.startTurn()
+        switchPlayer(player1.name);
       } else {
         player2.score += total;
         $("#score2-span").text(player2.score);
         total = 0 ;
-        player2.endTurn();
-        player1.startTurn()
+        switchPlayer(player2.name);
       }
     });
-  });
-
 });
