ddd40fbe84aaad77573bb082cc4446e3df6caaa6
Rony Nasr <nasr.rony@gmail.com>
2016-06-15 00:42:14 -0700

    added backend logic

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,10 @@ Player.prototype.hold = function (total) {
   this.score += total;
 };
 
-
+// function Game(){
+//   this.total= 0
+//   this.players = [];
+// }
 
 //Front end logic
 $(function(){
@@ -35,6 +38,9 @@ $(function(){
 
     $("#p1-score").text(player1.name);
     $("#p2-score").text(player2.name);
+    $("#score1-span").text(player1.score);
+    $("#score2-span").text(player2.score);
+    $("#total-span").text(total);
 
     $(".game-col").show();
   });
@@ -44,13 +50,34 @@ $(function(){
       var dice = getRandom(1, 6);
       if (dice !== 1){
         total +=dice;
+        $("#total-span").text(total);
       }else {
         total=0;
-        endTurn();
+        $("#total-span").text(total);
+        if (player1.turn){
+          player1.endTurn();
+          player2.startTurn()
+        }else {
+          player2.endTurn();
+          player1.startTurn()
+        }
       }
 
     $("#hold").click(function() {
-      endTurn();
+    //  debugger;
+      if (player1.turn === 1){
+        player1.score += total;
+        $("#score1-span").text(player1.score);
+        total = 0 ;
+        player1.endTurn();
+        player2.startTurn()
+      } else {
+        player2.score += total;
+        $("#score2-span").text(player2.score);
+        total = 0 ;
+        player2.endTurn();
+        player1.startTurn()
+      }
     });
   });
 
